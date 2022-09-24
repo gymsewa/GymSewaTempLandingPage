292cdf08196681d8b06d4718144c5f616fc0ee36
mamertmg <marquezgarcia.a@gmail.com>
2022-09-24 15:16:29 +0200

    added code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Header, Banner, About, Workouts, Pricing, Community, Faq, Join, Footer} from './components/index'
 
-function App() {
+// import aos
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
+
+const App = () => {
+
+  // aos initialization
+  Aos.init({
+      duration: 2500,
+      delay: 400,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='max-w-[1440px] mx-auto bg-page overflow-hidden relative'>
+      <Header/>
+      <Banner/>
+      <About/>
+      <Workouts/>
+      <Pricing/>
+      <Community/>
+      <Faq/>
+      <Join/>
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
